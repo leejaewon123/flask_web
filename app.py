136b853746375c57edad403f588d60bc8f1fcd93
Won <wow5wow5@naver.com>
2021-04-19 17:28:29 +0900

    article

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template
-
+from data import Articles
 
 app = Flask(__name__)
 
@@ -16,8 +16,17 @@ def about():
   return render_template("about.html", hello = "gary kim")
 
 @app.route('/articles')
-def article():
-    return render_template("articles.html", hello = "gary kim")
+def articleS():
+  articles = Articles()
+  print(articles)
+  return render_template("articles.html", articles = articles)
+
+@app.route('/article/<int:id>')
+def article(id):
+  articles = Articles()
+  article = articles[id-1]
+  print(articles[id-1])
+  return render_template("article.html" , article = article)
 
 if __name__ == '__main__':
     app.run()
